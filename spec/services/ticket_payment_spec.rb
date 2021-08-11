ef185fccb9de7f03a8fbcb3aa7abe975aaddd6de
Tommy5230 <tomasz.donarski@naturaily.com>
2021-08-11 22:18:08 +0200

    Fix ticket payment spec

--- spec/services/ticket_payment_spec.rb
+++ spec/services/ticket_payment_spec.rb
@@ -2,31 +2,63 @@
 
 RSpec.describe TicketPayment do
   describe ".call" do
-    subject { described_class.call(ticket, token, tickets_count) }
+    # subject { described_class.call(ticket, token, tickets_count) }
+    subject { described_class.call(reservation, token) }
 
-    let(:ticket) { create(:ticket) }
+    let(:reservation) { create(:reservation) }
+    let(:event) { create(:event) }
     let(:token) { "token" }
 
 
-    context "when tickets are available" do
-      let(:tickets_count) { 1 }
+    # context "when tickets are available" do
+    #   let(:tickets_count) { 1 }
+    #
+    #   it "should call payment adapter" do
+    #     expect(Payment::Gateway).to receive(:charge).with(amount: ticket.price, token: token)
+    #     subject
+    #   end
+    #
+    #   it "should update available tickets count" do
+    #     expect { subject }.to change(ticket, :available).by(-1)
+    #   end
+    # end
 
-      it "should call payment adapter" do
-        expect(Payment::Gateway).to receive(:charge).with(amount: ticket.price, token: token)
-        subject
-      end
+    # context "when tickets are not available" do
+    #   let(:tickets_count) { ticket.available + 1 }
+    #
+    #   it "should raise error" do
+    #     expect { subject }.to raise_error(TicketPayment::NotEnoughTicketsError)
+    #   end
+    # end
 
-      it "should update available tickets count" do
-        expect { subject }.to change(ticket, :available).by(-1)
-      end
-    end
-
-    context "when tickets are not available" do
-      let(:tickets_count) { ticket.available + 1 }
+    context "when reservation is paid" do
+      let(:reservation) { create(:reservation, :paid) }
 
       it "should raise error" do
-        expect { subject }.to raise_error(TicketPayment::NotEnoughTicketsError)
+        expect { subject }.to raise_error(TicketPayment::ReservationPaid)
       end
     end
+
+    context "when reservation has expired" do
+      let(:reservation) { create(:reservation, :expired) }
+
+      it "should raise error" do
+        expect { subject }.to raise_error(TicketPayment::ReservationExpired)
+      end
+    end
+
+    # context "when reservation is neither paid nor expired" do
+    #   let(:ticket) { create(:ticket, event: event, available: 5, reserved: 3, sold: 0) }
+    #
+    #   it "should call payment adapter" do
+    #     expect(Payment::Gateway).to receive(:charge).with(amount: ticket.price, token: token)
+    #     subject
+    #   end
+    #
+    #   it "should update ticket and reservation" do
+    #     expect { subject }.to change { ticket.reload.reserved }.from(3).to(2)
+    #       .and change { ticket.reload.sold }.from(0).to(1)
+    #   end
+    # end
   end
 end
